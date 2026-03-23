b853433a1156cafcdd53435374f10b088b772311
kanaloa2 <50844297+kanaloa2@users.noreply.github.com>
2026-03-22 18:46:45 -0700

    Update Supabase key in supabase-config.js
    Updated the Supabase key in the configuration file.

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // supabase-config.js
 const SUPABASE_URL = "https://rytmoxxpoegllfbusykt.supabase.co";
-const SUPABASE_KEY = "PASTE_YOUR_SB_PUBLISHABLE_KEY_HERE";
+const SUPABASE_KEY = "sb_publishable_r86loDzli-AOYkWFDKbe9A_X9-uATdk";
 
 // This creates the connection "client"
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
