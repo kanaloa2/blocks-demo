c1f6bdfc62336f0a1ffcc73ef7a4d6b773e7a310
kanaloa2 <50844297+kanaloa2@users.noreply.github.com>
2026-03-22 18:17:41 -0700

    Add Supabase configuration file

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// supabase-config.js
+const SUPABASE_URL = "https://rytmoxxpoegllfbusykt.supabase.co";
+const SUPABASE_KEY = "PASTE_YOUR_SB_PUBLISHABLE_KEY_HERE";
+
+// This creates the connection "client"
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
